933054110c804f629a7978e590d83851739426d7
CARMA Developer <carma@i2-u39367sax.tfhrc2.gov>
2022-05-11 15:47:30 -0400

    updated build tena script for demo 1

--- scripts/build_tena_adapters/buildTenaAdapters.sh
+++ scripts/build_tena_adapters/buildTenaAdapters.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-#  *                                                                              
-#  * Copyright (C) 2022 LEIDOS.                                              
-#  *                                                                              
-#  * Licensed under the Apache License, Version 2.0 (the "License"); you may not  
-#  * use this file except in compliance with the License. You may obtain a copy o\
-# f                                                                               
-#  * the License at                                                               
-#  *                                                                              
-#  * http://www.apache.org/licenses/LICENSE-2.0                                   
-#  *                                                                              
-#  * Unless required by applicable law or agreed to in writing, software          
-#  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT    
-#  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the     
-#  * License for the specific language governing permissions and limitations under                                                                               
-#  * the License.                                                                 
-#  *
+##############################
+#	TODO
+# add download and build of VUG Threads
+
 
 username=$(whoami)
 
@@ -26,39 +14,30 @@ localInstallDir=/home/$username/tenadev/INSTALL		#location to install/build TENA
 #---------------------------------------------------------#
 
 #-------------------| TENA VARIABLES |-------------------#
-tenaVersion=6.0.7
-tenaBuildVersion=u1804-gcc75-64
+tenaVersion=6.0.8
+tenaBuildVersion=u2004-gcc9-64
 tenaSourceScript=$localTenaDir/$tenaVersion/scripts/tenaenv-$tenaBuildVersion-v$tenaVersion.sh
 #--------------------------------------------------------#
-/*                                                                              
- * Copyright (C) 2019-2021 LEIDOS.                                              
- *                                                                              
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not  
- * use this file except in compliance with the License. You may obtain a copy o\
-f                                                                               
- * the License at                                                               
- *                                                                              
- * http://www.apache.org/licenses/LICENSE-2.0                                   
- *                                                                              
- * Unless required by applicable law or agreed to in writing, software          
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT    
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the     
- * License for the specific language governing permissions and limitations unde\
-r                                                                               
- * the License.                                                                 
- */
+
 
 #-------------------| REMOTE VARIABLES |-------------------#
 #-------------------|  DO NOT CHANGE   |-------------------#
 remoteDownloadsDir=/home/Downloads 		#DO NOT CHANGE: internal docker directory mapped to localTenaPackageDownloadDir
 remoteTenaDir=/home/TENA			#DO NOT CHANGE: internal docker directory mapped to localTenaDir
+remoteInstallDir=/home/INSTALL		#DO NOT CHANGE: internal docker directory mapped to localInstallDir	
 #--------------------------------------------------------#
+
+middlewareVersion="MiddlewareSDK-v6.0.8"
+boostVersion="TENA_boost_1.77.0.1_Library"
+vugCombinedVersion="VUG-VOICES-Combined-v0.12.0"
+
 
 v2xhubGitUrl="https://github.com/usdot-fhwa-OPS/V2X-Hub.git"
 
-skipMake=false
-skipCmake=false
 skipDocker=false
+skipCmake=false
+skipMake=false
+
 
 
 if [[ -f $tenaSourceScript ]]; then
@@ -73,31 +52,49 @@ fi
 echo
 echo "What application would you like to install? [#]" 
 echo 
-echo "[1] carlaadapter"
-echo "[2] tenav2xhubgateway"
-echo "[3] tenaspatplugin"
+echo "[1] carla-tena-adapter"
+echo "[2] v2xhub-tena-bsm-plugin"
+echo "[3] v2xhub-tena-spat-plugin"
+echo "[4] v2xhub-tena-mobility-plugin"
+echo "[5] v2xhub-tena-traffic-control-plugin"
 echo
 read -p "--> " tenaAppIndex
 
 if [[ $tenaAppIndex == 1 ]]; then
-	tenaApp=carlaadapter
-	toInstall=("MiddlewareSDK-v6.0.7" "boost-v1.70.0.2" "VUG-Scenario-Distribution-v0.3.0" "VUG-Vehicle-Distribution-v0.4.0" "VUG-TrafficLight-Distribution-v0.2.0" "TENA-Geospatial-Waypoint-Distribution-v1.0.0")
-	gitCloneUrl="https://www.trmc.osd.mil/bitbucket/scm/vug/carlaadapter.git"
+	tenaApp=carla-tena-adapter
+	gitCloneUrl="https://www.trmc.osd.mil/bitbucket/scm/vug/carla-tena-adapter.git"
 	dockerContainer=tena:carla
 	remoteAppDir=/home/$tenaApp 			#DO NOT CHANGE: internal docker directory mapped to localAppDir
+	isV2xhubPlugin=false
+	
 elif [[ $tenaAppIndex == 2 ]]; then
-	tenaApp=tenav2xhubgateway
-	toInstall=("MiddlewareSDK-v6.0.7" "boost-v1.70.0.2" "VUG-Scenario-Distribution-v0.3.0" "VUG-Vehicle-Distribution-v0.4.0" )
-	gitCloneUrl="https://www.trmc.osd.mil/bitbucket/scm/vug/tenav2xhubgateway.git"
+	tenaApp=v2xhub-tena-bsm-plugin
+	gitCloneUrl="https://www.trmc.osd.mil/bitbucket/scm/vug/v2xhub-tena-bsm-plugin.git"
 	dockerContainer=tena:v2xhub
 	remoteAppDir=/home/V2X-Hub/src/$tenaApp	#DO NOT CHANGE: internal docker directory mapped to localAppDir
+	isV2xhubPlugin=true
 	
 elif [[ $tenaAppIndex == 3 ]]; then
-	tenaApp=tenaspatplugin
-	toInstall=("MiddlewareSDK-v6.0.7" "boost-v1.70.0.2" "VUG-Scenario-Distribution-v0.3.0" "VUG-Vehicle-Distribution-v0.4.0" )
-	gitCloneUrl="https://www.trmc.osd.mil/bitbucket/scm/vug/tenaspatplugin.git"
+	tenaApp=v2xhub-tena-spat-plugin
+	gitCloneUrl="https://www.trmc.osd.mil/bitbucket/scm/vug/v2xhub-tena-spat-plugin.git"
 	dockerContainer=tena:v2xhub
 	remoteAppDir=/home/V2X-Hub/src/$tenaApp	#DO NOT CHANGE: internal docker directory mapped to localAppDir
+	isV2xhubPlugin=true
+	
+elif [[ $tenaAppIndex == 4 ]]; then
+	tenaApp=v2xhub-tena-mobility-plugin
+	gitCloneUrl="https://www.trmc.osd.mil/bitbucket/scm/vug/v2xhub-tena-mobility-plugin.git"
+	dockerContainer=tena:v2xhub
+	remoteAppDir=/home/V2X-Hub/src/$tenaApp	#DO NOT CHANGE: internal docker directory mapped to localAppDir
+	isV2xhubPlugin=true
+	
+elif [[ $tenaAppIndex == 5 ]]; then
+	tenaApp=v2xhub-tena-traffic-control-plugin
+	gitCloneUrl="https://www.trmc.osd.mil/bitbucket/scm/vug/v2xhub-tena-traffic-control-plugin.git"
+	dockerContainer=tena:v2xhub
+	remoteAppDir=/home/V2X-Hub/src/$tenaApp	#DO NOT CHANGE: internal docker directory mapped to localAppDir
+	isV2xhubPlugin=true
+
 else
 	echo "Invalid selection, try again..."
 	exit
@@ -110,8 +107,15 @@ echo
 if [[ ! -d $localAppDir ]]; then
 	read -p "Application directory not found. Would you like to download from BitBucket? [y/n] " downloadApp
 	
+	read -p "What branch would you like to use? [leave blank for develop] " branchToDownload
+	
 	if [[ $downloadApp =~ ^[yY]$ ]]; then
-		git clone $gitCloneUrl $localAppDir
+	
+		if [[ $branchToDownload == "" ]]; then
+			git clone $gitCloneUrl -b develop $localAppDir
+		else
+			git clone $gitCloneUrl -b $branchToDownload $localAppDir
+		fi
 	else
 		echo "Please clone the latest application directory from BitBucket..."
 	fi
@@ -119,47 +123,35 @@ else
 	echo "$tenaApp directory found"
 fi
 
-echo
-if ([[ $tenaApp == "tenav2xhubgateway" ]] || [[ $tenaApp == "tenaspatplugin" ]] ) && [[ ! -d $localInstallDir/V2X-Hub ]]; then
-	read -p "V2X-Hub directory not found. Would you like to download from GitHub? [y/n] " downloadApp
-	
-	if [[ $downloadApp =~ ^[yY]$ ]]; then
-		git clone $v2xhubGitUrl
-	else
-		echo "Please clone the latest V2X-Hub directory from Github..."
-	fi
-else
-	echo "V2X-Hub directory found"
-fi
 
-
-
-availableInstallers=($localTenaPackageDownloadDir/*.bin)
+##### Look for required packages to be isntalled
 
 echo
 echo "The looking for packages to be installed:"
 echo
 
 
-allPackagesFound=true
+# look for middleware
+if [ -d $localTenaDir/$tenaVersion ]; then
+	echo "TENA Middleware $tenaVersion found..."
+else
+	echo "The proper TENA Middleware version was not found. Please install version $tenaVersion"
+	exit
+fi
 
-for f in "${toInstall[@]}"; do
-	packageFound=false
-	for i in "${availableInstallers[@]}"; do
-		if grep -q "$f" <<<"$i"; then
-			echo "Package $f found..."
-			packageFound=true
-		fi
-	done
-	if [[ $packageFound == false ]]; then
-		allPackagesFound=false
-		echo "[!!!] Package $f not found..."
-	fi
-done
+#look for boost
+if [ -d $localTenaDir/$boostVersion* ]; then
+	echo "$boostVersion found..."
+else
+	echo "The proper Boost version was not found. Please install version $boostVersion"
+	exit
+fi
 
-if  [[ $allPackagesFound == false ]]; then
-	echo
-	echo "Not all packages found, exiting..." 
+#look for VUG Combined
+if [ -d $localTenaDir/$tenaVersion/src/$vugCombinedVersion* ]; then
+	echo "$vugCombinedVersion found..."
+else
+	echo "The proper VUG-Combined was not found. Please install version $vugCombinedVersion"
 	exit
 fi
 
@@ -174,16 +166,21 @@ for arg in "$@"; do
 	esac
 done
 
-currentDockerImages=$(sudo docker image list -q $dockerContainer)
-rebuildContainer=true
 
-if [[ $skipDocker == false ]] && [[ -n $currentDockerImages ]]; then
-	echo
-	read -p "Container $dockerContainer already exists, would you like to rebuild? [y/n] " rebuildContainerYn
-	if [[ $rebuildContainerYn =~ ^[yY]$ ]]; then
-		skipDocker=false
-	else
-		skipDocker=true
+
+if [[ $skipDocker == false ]] ; then
+	
+	currentDockerImages=$(sudo docker image list -q $dockerContainer)
+	
+	if [[ -n $currentDockerImages ]] ; then
+	
+		echo
+		read -p "Container $dockerContainer already exists, would you like to rebuild? [y/n] " rebuildContainerYn
+		if [[ $rebuildContainerYn =~ ^[yY]$ ]]; then
+			skipDocker=false
+		else
+			skipDocker=true
+		fi
 	fi
 fi
 
@@ -197,8 +194,30 @@ if [[ "$skipDocker" == true ]]
 		
 		sudo docker rm -v $dockerContainer 
 		sudo docker image rm $dockerContainer
+		
+		#if v2xhub plugin need to build image inside the V2X-Hub dir
+		if [ isV2xhubPlugin ]; then
+			
+			#if the v2xhub directory doesnt exist, download it
+			if [[ ! -d ./V2X-Hub ]]; then
 				
-		sudo docker build --force-rm --rm -f $localAppDir/docker/Dockerfile -t $dockerContainer .
+				echo
+				read -p "V2X-Hub directory not found. Would you like to download from GitHub? [y/n] " downloadApp
+				if [[ $downloadApp =~ ^[yY]$ ]]; then
+					git clone $v2xhubGitUrl
+				else
+					echo "Please clone the latest V2X-Hub directory from Github..."
+					exit
+				fi
+			else
+				echo "V2X-Hub directory found"
+			fi
+			
+			cd ./V2X-Hub
+		fi
+				
+		sudo -E docker build --force-rm --rm -f $localAppDir/docker/Dockerfile -t $dockerContainer .
+		
 		
 		
 		currentDockerImages=$(sudo docker image list -q $dockerContainer)
@@ -209,15 +228,11 @@ if [[ "$skipDocker" == true ]]
 			exit
 		fi
 		
-		echo
-		echo "#### TENA Components Install ####"
-		for f in "${availableInstallers[@]}"; do
-			for i in "${toInstall[@]}"; do
-				if grep -q "$i" <<<"$f"; then
-					sudo docker run --rm -v $localTenaPackageDownloadDir:$remoteDownloadsDir -v $localTenaDir:$remoteTenaDir $dockerContainer $remoteDownloadsDir/$(basename $f) -i /home --auto
-				fi
-			done
-		done
+		#if v2xhub plugin go out of the v2xhub directory and remove it
+		if [ isV2xhubPlugin ]; then
+			cd ../
+			sudo rm -rf ./V2X-Hub
+		fi
 fi
 
 
@@ -260,10 +275,14 @@ if [[ "$skipCmake" == true ]]
 		
 		
 		echo "CMAKE COMMAND: "
-		echo docker run --rm -v $localAppDir:$remoteAppDir -v $localTenaDir:$remoteTenaDir $dockerContainer bash -c "cd $remoteAppDir/build; export TENA_PLATFORM=u1804-gcc75-64; export TENA_HOME=$remoteTenaDir; export TENA_VERSION=6.0.7; export CARLA_HOME=/home/carla; cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=$remoteAppDir/INSTALL -DCMAKE_PREFIX_PATH=$remoteTenaDir/lib/cmake -D BOOST_INCLUDEDIR=$remoteTenaDir/TENA_boost_1.70.0.2_Library/u1804-gcc75-64/include -D VUG_INSTALL_DIR=$remoteAppDir/INSTALL ../"
+		#echo docker run --rm -v $localAppDir:$remoteAppDir -v $localTenaDir:$remoteTenaDir -v $localInstallDir:$remoteInstallDir $dockerContainer bash -c "cd $remoteAppDir/build; export TENA_PLATFORM=$tenaBuildVersion; export TENA_HOME=$remoteTenaDir; export TENA_VERSION=6.0.8; export CARLA_HOME=/home/carla; cmake -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -D CMAKE_INSTALL_PREFIX=$remoteAppDir/INSTALL -D CMAKE_PREFIX_PATH='$remoteTenaDir/lib/cmake;$remoteInstallDir' -D BOOST_INCLUDEDIR=$remoteTenaDir/$boostVersion/$tenaBuildVersion/include -D VUG_INSTALL_DIR=$remoteAppDir/INSTALL ../"
+		RUN git clone https://github.com/usdot-fhwa-OPS/libwebsockets.git
+
+		#-D vug-threads_DIR=$remoteInstallDir/vug-threads-2.2.0/include/cmake
+		
 		echo
 		
-		sudo docker run --rm -v $localAppDir:$remoteAppDir -v $localTenaDir:$remoteTenaDir $dockerContainer bash -c "cd $remoteAppDir/build; export TENA_PLATFORM=u1804-gcc75-64; export TENA_HOME=$remoteTenaDir; export TENA_VERSION=6.0.7; export CARLA_HOME=/home/carla; cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=$remoteAppDir/INSTALL -DCMAKE_PREFIX_PATH=$remoteTenaDir/lib/cmake -D BOOST_INCLUDEDIR=$remoteTenaDir/TENA_boost_1.70.0.2_Library/u1804-gcc75-64/include -D VUG_INSTALL_DIR=$remoteAppDir/INSTALL ../"
+		( set -x ; sudo docker run --rm -v $localAppDir:$remoteAppDir -v $localTenaDir:$remoteTenaDir -v $localInstallDir:$remoteInstallDir $dockerContainer bash -c "cd $remoteAppDir/build; export TENA_PLATFORM=$tenaBuildVersion; export TENA_HOME=$remoteTenaDir; export TENA_VERSION=6.0.8; export CARLA_HOME=/home/carla; cmake -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -D CMAKE_INSTALL_PREFIX=$remoteAppDir/$tenaApp -D CMAKE_PREFIX_PATH='$remoteTenaDir/lib/cmake;$remoteInstallDir' -D BOOST_INCLUDEDIR=$remoteTenaDir/$boostVersion/$tenaBuildVersion/include -D VUG_INSTALL_DIR=$remoteAppDir/INSTALL ../" )
 fi
 
 #--Make example
@@ -280,7 +299,17 @@ if [[ "$skipMake" == true ]]
 		
 		echo
 		echo "MAKE COMMAND: "
-		echo docker run --rm -v $localAppDir:$remoteAppDir -v $localTenaDir:$remoteTenaDir $dockerContainer bash -c "cd $remoteAppDir/build; export TENA_PLATFORM=u1804-gcc75-64; export TENA_HOME=$remoteTenaDir; export TENA_VERSION=6.0.7; export CARLA_HOME=/home/carla; make VERBOSE=1"
+		#echo docker run --rm -v $localAppDir:$remoteAppDir -v $localTenaDir:$remoteTenaDir $dockerContainer bash -c "cd $remoteAppDir/build; export TENA_PLATFORM=$tenaBuildVersion; export TENA_HOME=$remoteTenaDir; export TENA_VERSION=6.0.8; export CARLA_HOME=/home/carla; make VERBOSE=1"
 		echo
-		sudo docker run --rm -v $localAppDir:$remoteAppDir -v $localTenaDir:$remoteTenaDir $dockerContainer bash -c "cd $remoteAppDir/build; export TENA_PLATFORM=u1804-gcc75-64; export TENA_HOME=$remoteTenaDir; export TENA_VERSION=6.0.7; export CARLA_HOME=/home/carla; make VERBOSE=1"
+		( set -x ; sudo docker run --rm -v $localAppDir:$remoteAppDir -v $localTenaDir:$remoteTenaDir -v $localInstallDir:$remoteInstallDir $dockerContainer bash -c "cd $remoteAppDir/build; export TENA_PLATFORM=$tenaBuildVersion; export TENA_HOME=$remoteTenaDir; export TENA_VERSION=6.0.8; export CARLA_HOME=/home/carla; make VERBOSE=1" )
+		
+		if [ isV2xhubPlugin ]; then
+		
+			echo
+			echo "#### Running Make Install ####"
+			
+			echo
+			echo "MAKE INSTALL COMMAND: "
+			( set -x ; sudo docker run --rm -v $localAppDir:$remoteAppDir -v $localTenaDir:$remoteTenaDir -v $localInstallDir:$remoteInstallDir $dockerContainer bash -c "cd $remoteAppDir/build; export TENA_PLATFORM=$tenaBuildVersion; export TENA_HOME=$remoteTenaDir; export TENA_VERSION=6.0.8; export CARLA_HOME=/home/carla; make install VERBOSE=1" )
+		fi
 fi
