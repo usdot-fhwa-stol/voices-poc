adba6283af9bad4f044b92c1764500bf0b3e28f6
CARMA Developer <carma@i2-u39367sax.tfhrc2.gov>
2022-05-11 15:23:16 -0400

    add licenses to files

--- scripts/build_tena_adapters/buildTenaAdapters.sh
+++ scripts/build_tena_adapters/buildTenaAdapters.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#  *                                                                              
+#  * Copyright (C) 2022 LEIDOS.                                              
+#  *                                                                              
+#  * Licensed under the Apache License, Version 2.0 (the "License"); you may not  
+#  * use this file except in compliance with the License. You may obtain a copy o\
+# f                                                                               
+#  * the License at                                                               
+#  *                                                                              
+#  * http://www.apache.org/licenses/LICENSE-2.0                                   
+#  *                                                                              
+#  * Unless required by applicable law or agreed to in writing, software          
+#  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT    
+#  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the     
+#  * License for the specific language governing permissions and limitations under                                                                               
+#  * the License.                                                                 
+#  *
 
 username=$(whoami)
 
@@ -14,7 +30,23 @@ tenaVersion=6.0.7
 tenaBuildVersion=u1804-gcc75-64
 tenaSourceScript=$localTenaDir/$tenaVersion/scripts/tenaenv-$tenaBuildVersion-v$tenaVersion.sh
 #--------------------------------------------------------#
-
+/*                                                                              
+ * Copyright (C) 2019-2021 LEIDOS.                                              
+ *                                                                              
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not  
+ * use this file except in compliance with the License. You may obtain a copy o\
+f                                                                               
+ * the License at                                                               
+ *                                                                              
+ * http://www.apache.org/licenses/LICENSE-2.0                                   
+ *                                                                              
+ * Unless required by applicable law or agreed to in writing, software          
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT    
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the     
+ * License for the specific language governing permissions and limitations unde\
+r                                                                               
+ * the License.                                                                 
+ */
 
 #-------------------| REMOTE VARIABLES |-------------------#
 #-------------------|  DO NOT CHANGE   |-------------------#
