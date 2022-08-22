7c6453ed9bdc55152d037051c5eadc7d9b6f9aae
Andrew Loughran <andrew.j.loughran@leidos.com>
2022-08-22 18:19:22 -0400

    major changes including ability to decode all message types at once

--- scripts/J2735_pcap_decoder/src/J2735_pcap_decoder.sh
+++ scripts/J2735_pcap_decoder/src/J2735_pcap_decoder.sh
@@ -20,6 +20,7 @@
 #exit on error
 #
 set -e
+# set -x
 
 #this takes the location of the executed script as opposed to the current location
 #this allows it to be run anywhere
@@ -97,6 +98,7 @@ echo "Message Type ID: "$message_type_id
 payloadType="hex"
 messageTypeIdFound=false
 
+
 searchForMessageTypeIdInFile(){
 fileToCheck=$1
 messageTypeIdToFind=$2
@@ -126,83 +128,83 @@ ls *.pcap
 echo 
 
 while true; do
-	read -rep "Input filename from list: " file_to_read
-	file_to_write="${file_to_read//pcap/csv}"
-	if [ ! -f $file_to_read ]; then
+	read -rep "Input filename from list: " pcap_file_to_read
+	if [ ! -f $pcap_file_to_read ]; then
 		echo "File not found!"
 	else
 		break
 	fi
 done
 
+input_pcap_base_name=${pcap_file_to_read%".pcap"}
+#echo $input_pcap_base_name
 
-#tshark -r $file_to_read --disable-protocol wsmp -Tfields -Eseparator=, -e frame.time_epoch -e data.data > $directory/data/tsharkOutput/$file_to_write
+#tshark -r $pcap_file_to_read --disable-protocol wsmp -Tfields -Eseparator=, -e frame.time_epoch -e data.data > $directory/data/tsharkOutput/$file_to_write
 
 #searchForMessageTypeIdInFile $directory/data/tsharkOutput/$file_to_write $message_type_id 10
 
 #search first 10 packets in text format and look for Payload=
 #if this exist, string, we are ascii payloads and can decode that way
 #if not, we must grab the payload from the frame_raw (tshark_json_parser)
-search_for_ascii=$(tshark -r $file_to_read -o data.show_as_text:TRUE --disable-protocol wsmp -T fields -E separator=, -e frame.time_epoch -e data.text -c 10 | grep "Payload=")
-
-echo "Search: " $search_for_ascii
-
+search_for_ascii=$(tshark -r $pcap_file_to_read -o data.show_as_text:TRUE --disable-protocol wsmp -T fields -E separator=, -e data.text -c 10)
+#search_for_ascii=$(tshark -r $pcap_file_to_read -o data.show_as_text:TRUE --disable-protocol wsmp -T fields -E separator=, -e frame.time_epoch -e data.text -c 10 | grep "Payload=")
 
 
-if [  -z $search_for_ascii ]; then
+#echo "Search: " $search_for_ascii
+
+tshark_output_file=$input_pcap_base_name"_raw_packet_frame.csv"
+
+if [ true ]; then
+# if [[ $search_for_ascii = *[![:ascii:]]* ]]; then
         # The first packet contains the message id
 		printf "\nPCAP is HEX\n"
 
-		python3 $directory/src/tshark_json_parser.py $file_to_read $message_type_id
+
+		python3 $directory/src/tshark_json_parser.py $pcap_file_to_read $tshark_output_file
+		mv $tshark_output_file $directory/data/tsharkOutput
 
         printf "\nSuccessfully decoded pcap into hex payloads\n"
         payloadType="hex"
 else
         # The first packet does not contain the message id. This is most likely becuase payload is contained in ASCII
         printf "\nCould not find message ID in hex decoded payloads, trying ascii\n"
-        tshark -r $file_to_read -o data.show_as_text:TRUE --disable-protocol wsmp -T fields -E separator=, -e frame.time_epoch -e data.text > $directory/data/tsharkOutput/$file_to_write
+
+        # tshark -r $pcap_file_to_read -o data.show_as_text:TRUE --disable-protocol wsmp -T fields -E separator=, -e frame.time_epoch -e data.text > $directory/data/tsharkOutput/$raw_packet_output_file
+
+		python3 $directory/src/tshark_ascii_parser.py $pcap_file_to_read $tshark_output_file
+		mv $tshark_output_file $directory/data/tsharkOutput
+
+        #searchForMessageTypeIdInFile $directory/data/tsharkOutput/$raw_packet_output_file $message_type_id 10
+        
+        printf "\nSuccessfully decoded pcap into ascii payloads\n\n"
         payloadType="ascii"
-        
-        searchForMessageTypeIdInFile $directory/data/tsharkOutput/$file_to_write $message_type_id 10
-        
-        if [ $messageTypeIdFound ]; then
-        	printf "\nSuccessfully decoded pcap into ascii payloads\n\n"
-        else
-        	printf "\nDecoding in ascii still resulted in an empty file, exiting...\n"
-        	exit
-        fi
-        
 fi
 
-exit 
+# echo
+# echo "----- EARLY EXIT FOR TESTING -----"
+# exit 
 
 }
 
 
 getPayload(){
-  cd $directory/data/tsharkOutput
-  #parse tshark output to get rid of unnecessary bytes in front of payloads
-  for i in *
-  do
-    python3 $directory/src/tshark_OutputParser.py $i $message_type $payloadType
-  done
-  mv *_payload.csv $directory/data/payloadOutput
+	cd $directory/data/tsharkOutput
+	parsed_tshark_file=$input_pcap_base_name"_filtered_packets.csv"
+	#parse tshark output to get rid of unnecessary bytes in front of payloads
+	python3 $directory/src/tshark_OutputParser.py $tshark_output_file $parsed_tshark_file $message_type_id
+	mv $parsed_tshark_file $directory/data/payloadOutput
 }
 
 
 decodePackets(){
-  cd $directory/data/payloadOutput
-  printf 'Decoding...\n'
-  for i in $(find . -name "*.csv")
-  do
-    file=$(basename -- "$i")
-    fileName="decoded_${file%.*}.csv"
-	echo "$fileName"
-    python3 $directory/src/J2735decoder.py $i ${fileName} "$message_type" $message_type_id
-  done
+	cd $directory/data/payloadOutput
+	printf 'Decoding...\n'
 
-  mv *decoded* $directory/data/decodedOutput
-  printf 'Complete.\n'
+	decoded_file=$input_pcap_base_name"_decoded.csv"
+	python3 $directory/src/J2735decoder.py $parsed_tshark_file $decoded_file $message_type_id
+
+	mv $decoded_file $directory/data/decodedOutput
+	printf 'Complete.\n'
 }
 
 
@@ -284,3 +286,5 @@ processing(){
 }
 
 processing
+
+trap 'ERROR: $LINENOE' ERR
--- scripts/J2735_pcap_decoder/src/tshark_OutputParser.py
+++ scripts/J2735_pcap_decoder/src/tshark_OutputParser.py
@@ -21,49 +21,156 @@
 import json
 import sys
 from csv import reader, writer
+import re
 
+print("\nARG LIST:")
+for i, arg in enumerate(sys.argv):
+    print(str(i) + ": " + str(arg))
+print("")
 
 #print("ARGS: " + sys.argv[0] + "," + sys.argv[1] + "," + sys.argv[2] + "," + sys.argv[3] + "," + sys.argv[4])
 inFile = sys.argv[1]
-payloadType=sys.argv[4]
-print("Payload Type: " + payloadType)
-fileName = inFile.split(".")
+outfile = sys.argv[2]
+# payload_type_id=sys.argv[3]
 
-with open(fileName[0]+'.csv') as read_obj, \
-open(fileName[0]+'_payload.csv', 'w', newline='') as write_obj:
+
+message_type_list = [
+    {
+        'name'  : 'MAP',
+        'id'    : '0012'
+    },
+    {
+        'name'  : 'SPAT',
+        'id'    : '0013'
+    },
+    {
+        'name'  : 'BSM',
+        'id'    : '0014'
+    },
+    {
+        'name'  : 'Mobility Request',
+        'id'    : '00f0'
+    },
+    {
+        'name'  : 'Mobility Response',
+        'id'    : '00f1'
+    },
+    {
+        'name'  : 'Mobility Path',
+        'id'    : '00f2'
+    },
+    {
+        'name'  : 'Mobility Operations',
+        'id'    : '00f3'
+    },
+    {
+        'name'  : 'TraffiC Control Request',
+        'id'    : '00f4'
+    },
+    {
+        'name'  : 'TraffiC Control Message',
+        'id'    : '00f5'
+    }
+]
+
+with open(inFile) as read_obj, \
+open(outfile, 'w', newline='') as write_obj:
     csv_reader = reader(read_obj)
     csv_writer = writer(write_obj)
 
-    substr = {
-        'map': '0012',
-        'spat':'0013',
-        'bsm': '0014',
-        'req': '00f0',
-        'res': '00f1',
-        'mop': '00f2',
-        'mom': '00f3',
-        'tcr': '00f4',
-        'tcm': '00f5'
-    }
+    # substr = {
+    #     'map': '0012',
+    #     'spat':'0013',
+    #     'bsm': '0014',
+    #     'req': '00f0',
+    #     'res': '00f1',
+    #     'mop': '00f2',
+    #     'mom': '00f3',
+    #     'tcr': '00f4',
+    #     'tcm': '00f5'
+    # }
+
+    # for row in csv_reader:
+    #     index = {}
+    #     smallerIndex = 10000
+        
+    #     #remove newlines for ascii
+    #     if payloadType == "ascii":
+    #         row[1] = row[1].replace("\\n","")
+        
+    #     for k in substr:
+    #         if substr[k] in row[1]:
+    #             index[k] = row[1].find(substr[k])
+    #             if index[k] < smallerIndex:
+    #                 smallerIndex = index[k]
+    #                 msg = row[1][smallerIndex:]
+        
+    #     csv_writer.writerow([row[0],msg])
+
+    found_packets = 0
+
+    csv_writer.writerow(["packet_index","timestamp","frame_raw","message_type","message_type_id","filtered_payload"])
+
+    #skip first row for headers
+    next(csv_reader)
 
     for row in csv_reader:
-        index = {}
-        smallerIndex = 10000
         
-        #remove newlines for ascii
-        if payloadType == "ascii":
-            row[1] = row[1].replace("\\n","")
-        
-        for k in substr:
-            if substr[k] in row[1]:
-                index[k] = row[1].find(substr[k])
-                if index[k] < smallerIndex:
-                    smallerIndex = index[k]
-                    msg = row[1][smallerIndex:]
-        
-        csv_writer.writerow([row[0],msg])
+        packet_index = row[0]
+        timestamp = row[1]
+        current_packet_frame_raw = row[2]
 
+        # print("packet_index: " + str(packet_index))
+        # print("timestamp: " + str(timestamp))
+        # print("current_packet_frame_raw: " + str(current_packet_frame_raw))
+        
+        #this is the message type id which appears closest to the header
+        #this starts at an arbitrary value
+        closest_message_type = {
+            "name"  : "NA",
+            "id"    : "0000",
+            "pos"   : 99999   
+        }
 
+        num_matching_message_types = 0
+
+        for message_type in message_type_list:
+            
+            j2735_payload_with_header_search = re.search('0380.*'+ message_type['id'],current_packet_frame_raw)
+
+            # print("Search: " + str(j2735_payload_with_header_search))
+            
+            if j2735_payload_with_header_search != None:
+                # print("Found: " + message_type['name'])
+                num_matching_message_types += 1
+
+                j2735_payload_with_header = str(j2735_payload_with_header_search.group(0))
+                search_result_start = j2735_payload_with_header_search.start()
+                search_result_end = j2735_payload_with_header_search.end()
+                # print("Start: " + str(search_result_start))
+                # print("End: " + str(search_result_end))
+                # print("With Header: " + str(j2735_payload_with_header))
+                
+                if search_result_end < closest_message_type["pos"]:
+                    closest_message_type["name"] = message_type['name']
+                    closest_message_type["id"] = message_type['id']
+                    closest_message_type["pos"] = search_result_end
+        # print("num_matching_message_types: " + str(num_matching_message_types))
+            
+                
+        # print("Closest Message Type: " + closest_message_type["name"])
+
+        final_j2735_payload_with_header_search = re.search('0380.*'+ closest_message_type['id'] + '.*',current_packet_frame_raw)
+        final_j2735_payload_with_header = str(final_j2735_payload_with_header_search.group(0))
+        final_j2735_payload_with_header_cleaned_search = re.search(closest_message_type['id'] + '.*',final_j2735_payload_with_header)
+        final_j2735_payload_with_header_cleaned = str(final_j2735_payload_with_header_cleaned_search.group(0))
+                
+        # print("Timestamp: " + timestamp)
+        # print("Clean: " + final_j2735_payload_with_header_cleaned)
+
+        csv_writer.writerow([packet_index,timestamp,current_packet_frame_raw,closest_message_type["name"],closest_message_type["id"],final_j2735_payload_with_header_cleaned])
+
+        # print("")
 
 
 # Old for Kapsch payloads. Testing with Kapsch payloads needed to include in updated 
