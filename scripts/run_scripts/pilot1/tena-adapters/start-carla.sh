bd9052c752f42dff433c927ddac863908280161c
Andrew Loughran <andrew.j.loughran@leidos.com>
2023-03-31 13:58:51 -0400

    updated run carla to move camera, updated simple vehicle model to use carla adapter spawned vehicle

--- scripts/run_scripts/pilot1/tena-adapters/start-carla.sh
+++ scripts/run_scripts/pilot1/tena-adapters/start-carla.sh
@@ -9,6 +9,13 @@ function cleanup {
 
 . ../../../../config/node_info.config
 
+if [[ -f $localCarlaPath/CarlaUE4.sh ]]; then
+	echo "Found CARLA Simulator"
+else
+	echo
+	echo "CARLA Simulator not found at $localCarlaPath/CarlaUE4.sh"
+	exit
+fi
 
 mkdir -p $localCarmaSimLogPath
 
@@ -22,6 +29,8 @@ $localCarlaPath/CarlaUE4.sh &>> $CARLA_LOG &
 carla_pid=$!
 echo "PID: "$carla_pid
 sleep 7s
+
+python3 $voicesPocPath/scripts/carla_python_scripts/spectator_veiw_town_04.py
 
 python3 $voicesPocPath/scripts/carla_python_scripts/set_time_mode.py
 
