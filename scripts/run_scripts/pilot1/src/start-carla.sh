3073ebd470f529c755a91e31e0e1a19db15cd8d9
Andrew Loughran <andrew.j.loughran@leidos.com>
2023-04-18 15:40:14 -0400

    Andrew - added map to start carla, change mode of EG

--- scripts/run_scripts/pilot1/src/start-carla.sh
+++ scripts/run_scripts/pilot1/src/start-carla.sh
@@ -17,9 +17,10 @@ function print_help {
 	echo "optional arguments:"
 	echo "    --no-tick          do not set time mode and tick simulation"
 	echo "    --low_quality      start CARLA in low quality mode"
+	echo "    --map <map name>   start carla with given map name"
 	echo "    --help             show help"
 }
-. ../../../config/node_info.config
+. ../../../../config/node_info.config
 
 if [[ -f $localCarlaPath/CarlaUE4.sh ]]; then
 	echo "Found CARLA Simulator"
@@ -39,17 +40,28 @@ CARLA_LOG=$localCarmaSimLogPath/voices_carla_simulator.log
 
 no_tick_enabled=false
 low_quality_flag=""
+next_flag_is_map=false
+carla_map=""
 
 
 for arg in "$@"
 do
-	if [[ $arg == "--no_tick" ]]; then
+	if [ "$next_flag_is_map" = true ]; then
+
+		carla_map=$arg
+		next_flag_is_map=false
+	
+	elif [[ $arg == "--no_tick" ]]; then
 		
 		no_tick_enabled=true
 
 	elif [[ $arg == "--low_quality" ]]; then
 		
 		low_quality_flag="-quality-level=Low"
+
+	elif [[ $arg == "--map" ]]; then
+		
+		next_flag_is_map=true
 
 	elif [[ $arg == "--help" ]]; then
 		
@@ -65,31 +77,33 @@ do
 	fi
 done
 
-if [ "$no_tick_enabled" = true ]; then
+$localCarlaPath/CarlaUE4.sh $low_quality_flag > $CARLA_LOG 2>&1 &
 
-	$localCarlaPath/CarlaUE4.sh $low_quality_flag > $CARLA_LOG 2>&1 &
+carla_pid=$!
+echo "CARLA PID: "$carla_pid
+sleep 7s
 
-	carla_pid=$!
-	echo "CARLA PID: "$carla_pid
-	sleep 7s
+if [[ $carla_map != "" ]]; then
+	echo "Changing map to: $carla_map"
+	python3 $voicesPocPath/scripts/carla_python_scripts/config.py -m $carla_map
+	sleep 5s
 
-	python3 $voicesPocPath/scripts/carla_python_scripts/blank_traffic_signals.py
+fi
+
+python3 $voicesPocPath/scripts/carla_python_scripts/blank_traffic_signals.py
+
+if [[ $carla_map == "Town04" ]]; then
 
 	python3 $voicesPocPath/scripts/carla_python_scripts/spectator_veiw_town_04.py
+	
+fi
+
+
+if [ "$no_tick_enabled" = true ]; then
 
 	read -s -n 1 -p "Press any key to quit . . ."
 
 else
-	
-	$localCarlaPath/CarlaUE4.sh $low_quality_flag > $CARLA_LOG 2>&1 &
-
-	carla_pid=$!
-	echo "CARLA PID: "$carla_pid
-	sleep 7s
-
-	python3 $voicesPocPath/scripts/carla_python_scripts/blank_traffic_signals.py
-
-	python3 $voicesPocPath/scripts/carla_python_scripts/spectator_veiw_town_04.py
 
 	python3 $voicesPocPath/scripts/carla_python_scripts/set_time_mode.py
 
