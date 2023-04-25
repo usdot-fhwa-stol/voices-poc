2286546bbc0731e982d9957e07e80c81d5e148d7
Andrew Loughran <andrew.j.loughran@leidos.com>
2023-04-25 15:55:56 -0400

    Minor script changes debugtging.

--- scripts/run_scripts/pilot1/src/start-carla-with-carma-integration.sh
+++ scripts/run_scripts/pilot1/src/start-carla-with-carma-integration.sh
@@ -5,7 +5,9 @@ function cleanup {
 	echo "Stopping CARMA Simulation"
 	docker kill carla_carma_integration
 	pkill -9 CarlaUE4
-#       kill -9 $set_time_mode_pid
+  if [[ $timeSyncEnabled == "true" ]]; then
+    kill -9 $set_time_mode_pid
+  fi
 	exit
 }
 
@@ -50,6 +52,7 @@ date >> $SIM_LOG
 
 
 no_tick_enabled=false
+timeSyncEnabled=false
 low_quality_flag=""
 next_flag_is_map=false
 carla_map=""
@@ -118,7 +121,7 @@ elif [[ $carla_map == "smart_intersection" ]]; then
 	sleep 5s
 
 	python3 $voicesPocPath/scripts/carla_python_scripts/spectator_view_smart_intersection.py
-
+  set -x
 	if [[ $carmaID == "TFHRC_CAR_2" ]]
 	then
 		SPAWN_PT="28.44,300.06,0,0,0,85" # latitude=34.067713, longitude=-118.445144, altitude=1.000000
@@ -142,36 +145,37 @@ fi
 python3 $voicesPocPath/scripts/carla_python_scripts/blank_traffic_signals.py
 
 
-# set time mode producing faster that real time clock, disabled for Pilot 1 tests 1-3
-#nohup python3 $voicesPocPath/scripts/carla_python_scripts/set_time_mode.py 2>&1 > $SET_TIME_MODE_LOG &
+if [[ $timeSyncEnabled == "true" ]]; then
+  # set time mode producing faster that real time clock, disabled for Pilot 1 tests 1-3
+  echo "Setting time mode."
+  nohup python3 $voicesPocPath/scripts/carla_python_scripts/set_time_mode.py 2>&1 > $SET_TIME_MODE_LOG &
+  set_time_mode_pid=$!
+  echo "Set time mode PID: "$set_time_mode_pid
+  echo
+  echo "----- SUCCESSFULLY SET TIME MODE, CONTINUOUSLY TICKING WORLD -----"
+  echo
+fi
 
-# set_time_mode_pid=$!
-echo "Set time mode PID: "$set_time_mode_pid
 
-echo
-echo "----- SUCCESSFULLY SET TIME MODE, CONTINUOUSLY TICKING WORLD -----"
-echo      
+echo "----- STARTING CARLA-CARMA INTEGRATION TOOL -----"
 
-set -x
 docker run \
 	   -it -d --rm \
        --name carma_carla_integration \
        --net=host \
        usdotfhwastol/carma-carla-integration:K900-test
 echo "------------------------exec---------------------------------"
+set -x
 docker exec \
-       -it \
-       carma_carla_integration \
-       bash -c \
-       "export PYTHONPATH=$PYTHONPATH:/home/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg && \
-    	source /home/carma_carla_ws/devel/setup.bash && \
-    	roslaunch carma_carla_agent carma_carla_agent.launch spawn_point:='28.44,300.06,0,0,0,85' town:='smart_intersection' selected_route:='UCLA' synchronous_mode:='true' speed_Kp:=0.4 speed_Ki:=0.03 speed_Kd:=0
-#       roslaunch carma_carla_agent carma_carla_agent.launch spawn_point:='$SPAWN_PT' role_name:='$carmaID' town:='$carla_map' selected_route:='UCLA' synchronous_mode:='false' speed_Kp:='0.4' speed_Ki:='0.03' speed_Kd:='0'"
-#       roslaunch carma_carla_agent carma_carla_agent.launch spawn_point:='$SPAWN_PT' role_name:='$carmaID' town:='$carla_map' selected_route:='Voices_Pilot1_Test4_TFHRC_CAR_2' synchronous_mode:='false' speed_Kp:='0.4' speed_Ki:='0.03' speed_Kd:='0'"
-#	     &> $SIM_LOG
+        -it \
+        carma_carla_integration \
+        bash -c \
+        "export PYTHONPATH=$PYTHONPATH:/home/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg && \
+        source /home/carma_carla_ws/devel/setup.bash && \
+        roslaunch carma_carla_agent carma_carla_agent.launch spawn_point:='28.44,300.06,0,0,0,85' town:='smart_intersection' selected_route:='UCLA' synchronous_mode:='true' speed_Kp:=0.4 speed_Ki:=0.03 speed_Kd:=0
+        &> $SIM_LOG
 
-
-
-
+# Should work but doesn't
+#roslaunch carma_carla_agent carma_carla_agent.launch spawn_point:='$SPAWN_PT' role_name:='$carmaID' town:='$carla_map' selected_route:='UCLA' synchronous_mode:='true' speed_Kp:='0.4' speed_Ki:='0.03' speed_Kd:='0'"
 
 cleanup
