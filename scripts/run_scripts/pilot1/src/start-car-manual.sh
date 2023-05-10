1ae9d9bce3f71c3bb751b56dd961696f013fa19f
Andrew Loughran <andrew.j.loughran@leidos.com>
2023-05-10 16:38:48 -0400

    Scripts.

---
+++ scripts/run_scripts/pilot1/src/start-car-manual.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Start Execution Manager
+
+. ../../../../config/node_info.config
+
+$voicesPocPath/scripts/carla_python_scripts/manual_control_keyboard.py  --rolename TFHRC-CAR-1  --x 34.348057  --y 274.059052  --z 3.457308
+
