44835e46c4c8e0e41308a5bc206f27de5aec6b15
Andrew Loughran <andrew.j.loughran@gmail.com>
2023-11-15 12:08:39 -0500

    syntax cleanup carla setup

--- docker/lib/COPY/start_scripts/setup-carla-docker.sh
+++ docker/lib/COPY/start_scripts/setup-carla-docker.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-env_set_site_config_path=/home/carla/voices-poc/config/site_config/$VUG_SITE_CONFIG_FILE
+env_set_site_config_path=$HOME/voices-poc/config/site_config/$VUG_SITE_CONFIG_FILE
 env_set_scenario_config_path=$HOME/voices-poc/config/scenario_config/$VUG_SCENARIO_CONFIG_FILE
 
 # the .voices_config setup is as follows in order to make existing scripts work within docker container: 
