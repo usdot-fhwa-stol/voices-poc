d2b0ba082da982ca4ddffc4157a3d9536163660f
Andrew Loughran <andrew.j.loughran@gmail.com>
2024-02-29 16:29:06 -0500

    added automatic detection of VPN tunnel

--- docker/pilot2-event2/start-pilot2-event2-docker.sh
+++ docker/pilot2-event2/start-pilot2-event2-docker.sh
@@ -69,6 +69,82 @@ else
     
 fi
 
+# tunnel_interface_pattern="tun[0-9]"
+tunnel_interface_pattern="enp60s[0-9]"
+tunnel_check=$(ip -br link show | awk '{print $1}' | grep -w "$tunnel_interface_pattern")
+
+if [[ ! -z $tunnel_check ]]; then
+    echo
+    echo "VPN tunnel interface found: $tunnel_check"
+
+    tunnel_local_ip=$(ip -br a show $tunnel_check | awk '{print $3}')
+    tunnel_local_ip_clean=${tunnel_local_ip%/*}
+    
+    if [[ "$tunnel_local_ip_clean" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
+        echo "    IP $tunnel_local_ip_clean is a valid IP"
+    else
+        echo
+        echo "Unable to automatically get tunnel local address"
+        read -p "Please enter your local IP address for the VPN tunnel (found under the tun interface using the command: 'ip -br a show' ) [###.###.###.###]: " manual_tunnel_local_ip
+        
+        if [[ "$manual_tunnel_local_ip" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
+            echo "    IP $manual_tunnel_local_ip is a valid IP"
+            tunnel_local_ip_clean=$manual_tunnel_local_ip
+        else
+            echo
+            echo "    IP $manual_tunnel_local_ip is NOT valid, please set VUG_LOCAL_ADDRESS manually in your site config"
+            tunnel_local_ip_clean=""
+        fi        
+    fi
+    
+    if [[ ! -z $tunnel_local_ip_clean ]]; then
+
+        echo
+        read -p "Would you like to use the tunnel IP as VUG_LOCAL_ADDRESS? ($tunnel_local_ip_clean) [y/n] " use_local_tunnel_ip
+    
+        if [[ $use_local_tunnel_ip =~ ^[yY]$ ]]; then
+
+            export VUG_VPN_LOCAL_ADDRESS=$tunnel_local_ip_clean
+        fi
+
+    fi
+
+    em_fqdn_address=$(getent hosts em.voices-network.local | awk '{print $1}')
+
+    if [[ "$em_fqdn_address" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
+        echo
+        echo "Automatically detected EM at $em_fqdn_address"
+    else
+        echo
+        echo "Unable to automatically get EM address"
+        read -p "Please enter the VPN EM Address (found in the VOICES Portal under Connection Information) [###.###.###.###]: " manual_tunnel_em_address
+        
+        if [[ "$manual_tunnel_em_address" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
+            echo "    IP $manual_tunnel_em_address is a valid IP"
+            em_fqdn_address=$manual_tunnel_em_address
+        else
+            echo
+            echo "    IP $manual_tunnel_em_address is NOT valid, please set VUG_LOCAL_ADDRESS manually in your site config"
+            em_fqdn_address=""
+        fi        
+    fi
+
+    if [[ ! -z $em_fqdn_address ]]; then
+
+        echo
+        read -p "Would you like to use the tunnel IP as VUG_EM_ADDRESS? ($em_fqdn_address) [y/n] " use_em_tunnel_ip
+    
+        if [[ $use_em_tunnel_ip =~ ^[yY]$ ]]; then
+
+            export VUG_VPN_EM_ADDRESS=$em_fqdn_address
+        fi
+
+    fi
+
+fi
+
+echo
+
 trap stopDocker SIGINT
 
 $docker_compose_cmd -f $docker_compose_file pull
