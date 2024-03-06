4d1d3d15c762d639b9863f131d4868825237d325
Andrew Loughran <andrew.j.loughran@gmail.com>
2024-03-06 14:11:13 -0500

    missed file

--- docker/other_scripts/stop_all_vehicles.sh
+++ docker/other_scripts/stop_all_vehicles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-docker exec -it --user root voices bash -c 'export HOME=/home && export VUG_CARLA_EGG_DIR=/home/CARLA_0.9.10/PythonAPI && cd $HOME/voices-poc/scripts/carla_python_scripts/ && python3 stop_vehicles.py'
+docker exec --user root voices bash -c 'export HOME=/home && export VUG_CARLA_EGG_DIR=/home/CARLA_0.9.10/PythonAPI && cd $HOME/voices-poc/scripts/carla_python_scripts/ && python3 stop_vehicles.py'
