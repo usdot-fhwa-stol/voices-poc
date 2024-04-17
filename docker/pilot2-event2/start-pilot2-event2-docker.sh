ebf02fd6c9200bd3dbdbe14de16f45b46e90402e
John Chrosniak <chrosniakj@gmail.com>
2024-04-17 14:16:42 -0400

    grab tun interfaces from output of sudo openvpn3 sessions-list

--- docker/pilot2-event2/start-pilot2-event2-docker.sh
+++ docker/pilot2-event2/start-pilot2-event2-docker.sh
@@ -82,8 +82,7 @@ fi
 final_vpn_local_address=""
 final_vpn_em_address=""
 
-vpn_interface_pattern="tun[0-9]"
-vpn_check=$(ip -br link show | awk '{print $1}' | grep -w "$vpn_interface_pattern")
+vpn_check=$(sudo openvpn3 sessions-list | grep -E tun[0-9])
 
 interfaces=($(ifconfig -a | grep -o '^[^ ]\+'))
 tun_interfaces=()
