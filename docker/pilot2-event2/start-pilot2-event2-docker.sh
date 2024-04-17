d85e01c83930ccd41c0759bf9b298f240ee8fa1f
John Chrosniak <chrosniakj@gmail.com>
2024-04-17 12:56:10 -0400

    fixed config name printing

--- docker/pilot2-event2/start-pilot2-event2-docker.sh
+++ docker/pilot2-event2/start-pilot2-event2-docker.sh
@@ -15,7 +15,7 @@ source $VUG_LOCAL_VOICES_POC_PATH/scripts/utils/stop_current_vpn_connection.sh
 if ! $VUG_LOCAL_VOICES_POC_PATH/scripts/utils/prune_vpn_connections.sh; then
     exit 1
 fi
- 
+
 
 voices_site_config=$HOME/.voices_site_config
 voices_scenario_config=$HOME/.voices_scenario_config
@@ -32,8 +32,8 @@ if [ -L ${voices_site_config} ] && [ -L ${voices_scenario_config} ]; then
         source $voices_scenario_config
         export VUG_SITE_CONFIG_FILE=$site_link_base_name
         export VUG_SCENARIO_CONFIG_FILE=$scenario_link_base_name
-        echo "Site Config: "$voices_site_config
-        echo "Scenario Config: "$voices_scenario_config
+        echo "Site Config: "$site_link_base_name
+        echo "Scenario Config: "$scenario_link_base_name
     else
         echo "[!!!] .voices_site_config or .voices_scenario_config link is broken"
         echo "Site Config: "$(readlink -f $voices_site_config)
@@ -59,13 +59,13 @@ docker_compose_v2_version=$(docker compose version 2> /dev/null)
 if [ ! -z "$docker_compose_v2_version" ]; then
 
     echo "docker compose version: "$docker_compose_v2_version
-    
+
     docker_compose_cmd="docker compose"
 else
     docker_compose_v1_version=$(docker-compose -v 2> /dev/null)
 
     if [ ! -z "$docker_compose_v1_version" ]; then
-        
+
         echo "docker-compose version: "$docker_compose_v1_version
 
         docker_compose_cmd="docker-compose"
@@ -73,7 +73,7 @@ else
         echo ERROR: No valid docker compose version found
         exit
     fi
-    
+
 fi
 
 final_vpn_local_address=""
@@ -96,7 +96,7 @@ done
 # Prompt if there are multiple tun interfaces
 if [ "${#tun_interfaces[@]}" -gt 1  ]
 then
-    echo '' 
+    echo ''
     echo "Multiple tunnel interfaces were found."
     for i in $(seq 1 ${#tun_interfaces[@]}); do
         echo "      " ${tun_interfaces[i-1]} ${tun_ip_addresses[i-1]}
@@ -111,7 +111,7 @@ then
     done
 elif [ "${#tun_interfaces[@]}" = 1  ]
 then
-    echo '' 
+    echo ''
     echo "A single tunnel interface was found."
     for i in $(seq 1 ${#tun_interfaces[@]}); do
         echo "      " ${tun_interfaces[i-1]} ${tun_ip_addresses[i-1]}
@@ -133,14 +133,14 @@ if [[ ! -z $vpn_check ]]; then
 
     vpn_local_ip=$(ip -br a show $vpn_check | awk '{print $3}')
     vpn_local_ip_clean=${vpn_local_ip%/*}
-    
+
     if [[ "$vpn_local_ip_clean" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
         echo "    IP $vpn_local_ip_clean is a valid IP"
     else
         echo
         echo "Unable to automatically get VPN local address"
         read -p "Please enter your local IP address for the VPN (found under the tun interface using the command: 'ip -br a show' ) [###.###.###.###]: " manual_vpn_local_ip
-        
+
         if [[ "$manual_vpn_local_ip" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
             echo "    IP $manual_vpn_local_ip is a valid IP"
             vpn_local_ip_clean=$manual_vpn_local_ip
@@ -148,9 +148,9 @@ if [[ ! -z $vpn_check ]]; then
             echo
             echo "    IP $manual_vpn_local_ip is NOT valid, please set VUG_LOCAL_ADDRESS manually in your site config"
             vpn_local_ip_clean=""
-        fi        
+        fi
     fi
-    
+
 
     em_fqdn_address=$(getent hosts em.voices-network.local | awk '{print $1}')
 
@@ -161,7 +161,7 @@ if [[ ! -z $vpn_check ]]; then
         echo
         echo "Unable to automatically get EM address"
         read -p "Please enter the VPN EM Address (found in the VOICES Portal under Connection Information) [###.###.###.###]: " manual_vpn_em_address
-        
+
         if [[ "$manual_vpn_em_address" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
             echo "    IP $manual_vpn_em_address is a valid IP"
             em_fqdn_address=$manual_vpn_em_address
@@ -169,7 +169,7 @@ if [[ ! -z $vpn_check ]]; then
             echo
             echo "    IP $manual_vpn_em_address is NOT valid, please set VUG_LOCAL_ADDRESS manually in your site config"
             em_fqdn_address=""
-        fi        
+        fi
     fi
 fi
 
