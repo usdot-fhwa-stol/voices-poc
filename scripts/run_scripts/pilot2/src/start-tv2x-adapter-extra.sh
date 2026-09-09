5650bfd304b85b5764e9b32d113739c93db40ae1
Rahul Andurkar <rahul.andurkar@yahoo.com>
2026-09-09 11:09:53 -0400

    testing

--- scripts/run_scripts/pilot2/src/start-tv2x-adapter-extra.sh
+++ scripts/run_scripts/pilot2/src/start-tv2x-adapter-extra.sh
@@ -67,7 +67,7 @@ fi
 
 localadapterPath=$VUG_LOCAL_INSTALL_PATH/$VUG_V2X_ADAPTER_VERSION
 
-adapterVerbosity='4'
+adapterVerbosity='1'
 
 useBestEffort=''
 if [[ $VUG_USE_BEST_EFFORT == true ]]; then
